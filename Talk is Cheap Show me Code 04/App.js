32168d2f15f7d56597c77f23cd79119969238e0c
Lucky <luckykashyap3430@gmail.com>
2023-12-29 15:52:20 +0530

    style header component

--- Talk is Cheap Show me Code 04/App.js
+++ Talk is Cheap Show me Code 04/App.js
@@ -2,8 +2,33 @@ import React from "react";
 
 import ReactDOM from "react-dom/client";
 
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://img.freepik.com/premium-vector/online-food-app-icon-food-shop-location-logo-also-online-resturent-location-template_608547-155.jpg?w=740"
+          alt="logo"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 const AppLayout = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
